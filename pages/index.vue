701e7fa3d26825956b32e1fd1d5583b50f20d315
cowlingj <09cowlingj@gmail.com>
2020-02-21 18:53:40 +0000

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="outer">
+    <div id="inner">
+      <div id="more-inner" ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  apollo: {
+    hello: {
+      skip: true,
+      query: gql`query { hello { name } }`,
+      error(err, vm) {
+        console.log('BOOM!')
+        console.log(err)
+        vm.err = err
+        // return true
+      },
+      errorPolicy: 'all'
+    }
+  }
+}
+</script>
